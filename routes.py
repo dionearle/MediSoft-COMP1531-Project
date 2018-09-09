ef9570bc4cb82a6e17490a7fd0506769aa63d1cd
Edward Webb <spider.pole@live.com>
2018-09-09 20:21:58 +1000

    Added username to top right corner

--- routes.py
+++ routes.py
@@ -3,6 +3,7 @@ from flask import request, render_template
 from users import User, Patient, Provider
 
 
+
 @app.route('/', methods=['POST', 'GET'])
 def index():
     if request.method == 'POST':
@@ -20,8 +21,8 @@ def index():
                     break
             
             if patient == True: 
-                userLoggedIn = Patient(email, password) # initialise Patient class
-                return render_template("loginPost.html", userType="Patient", success=True) # return html file
+                userLoggedIn = Patient(email, password, True) # initialise Patient class
+                return render_template("loginPost.html", userType="Patient", success=True, username=userLoggedIn.email) # return html file
         with open("provider.csv", "r") as w:
             provider = False # for detecting whether login failed or not later
             readLinesArray = w.readlines() # interpret data in provider.csv as an array
@@ -33,8 +34,8 @@ def index():
                     provider = True
                     break
             if provider == True:
-                userLoggedIn = Provider(name, password, specialisation) # initialise Provider class
-                return render_template("loginPost.html", userType="Provider", success=True) # return html file
+                userLoggedIn = Provider(name, password, specialisation, True) # initialise Provider class
+                return render_template("loginPost.html", userType="Provider", success=True, username=userLoggedIn.email) # return html file
 
         return render_template("loginPost.html", success=False) # if login failed, return an alert saying "Wrong email/password"
     return render_template("base.html") # runs when the user first opens the page
