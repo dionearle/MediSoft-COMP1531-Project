cdee7fedfe9471a3f04880d1a691c0afb5702395
Pipit21 <z5162791@student.unsw.edu.au>
2018-10-08 00:05:43 +1100

    improved times

--- routes.py
+++ routes.py
@@ -6,6 +6,7 @@ from search import searchFiles
 from appointmentManager import AppointmentManager
 from save import saveData
 import csv
+import datetime
 
 
 def get_user(user_id):
@@ -125,15 +126,14 @@ def update_profile(name):
 @app.route('/book/<email>', methods=['GET', 'POST'])
 @login_required
 def book(email):
-    times = ['00:00-00:30', '00:30-01:00', '01:00-01:30', '01:30-02:00', '02:00-02:30', '02:30-03:00',
-                '03:00-03:30', '03:30-04:00', '04:00-04:30', '04:30-05:00', '05:00-05:30', '05:30-06:00',
-                '06:00-06:30', '06:30-07:00', '07:00-07:30', '07:30-08:00', '08:00-08:30', '08:30-09:00',
-                '09:00-09:30', '09:30-10:00', '10:00-10:30', '10:30-11:00', '11:00-11:30', '11:30-12:00',
-
-                '12:00-12:30', '12:30-13:00', '13:00-13:30', '13:30-14:00', '14:00-14:30', '14:30-15:00',
-                '15:00-15:30', '15:30-16:00', '16:00-17:30', '16:30-17:00', '17:00-17:30', '17:30-18:00',
-                '18:00-18:30', '18:30-19:00', '19:00-20:30', '19:30-20:00', '20:00-20:30', '20:30-21:00',
-                '21:00-21:30', '21:30-22:00', '22:00-23:30', '22:30-23:00', '23:00-23:30', '23:30-00:00']
+    fmt='%H:%M'
+    time=datetime.datetime.strptime('00:00',fmt)
+    min30=datetime.timedelta(minutes=30)
+    times=[]
+    for i in range(48):
+        times.append('%s-%s' % ( time.strftime(fmt), (time+min30).strftime(fmt)))
+        time+=min30
+           
     return render_template('book.html', times=times, name=email)
 
 @app.route('/bookAppointment/<email>', methods=['GET', 'POST'])
