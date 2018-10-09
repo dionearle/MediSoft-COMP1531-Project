406819aa127496eca7063e9357db450a53f9b72a
Pipit21 <z5162791@student.unsw.edu.au>
2018-10-10 02:11:21 +1100

    Tried to add more providers & patients + documentation for tests.py

---
+++ tests.py
@@ -0,0 +1,31 @@
+#this will be where tests for user stories are conducted
+# According to the google doc
+    # 1: Book and Appointment
+    # US7
+    # "User-Story Description:
+        # As a patient, I want to book an appointment so I can receive healthcare service easily
+        # Acceptance Criteria:
+            # Enter required information including Medicare number into a form
+            # Be able to book an appointment through a healthcare provider?s or centre?s profile page
+            # Have an option of 48 equally sized time slots, each of which is 30 minutes long
+            # Be able to enter brief information for the appointment
+            # Be able to display a confirmation that the appointment has been booked to the user
+
+    # 2: View Patient History
+    # US13
+    # User-Story Description:
+        # As a patient, I want to be able to view my own appointments history so I can easily access which appointments I have recently attended
+        # Acceptance Criteria:
+            # The list of appointments should have links to the profile pages of the corresponding health provider and healthcare centre
+            # The list of appointments should be chronological and listed with the date of appointment
+            
+    # US15
+    # User-Story Description:
+        # As a healthcare provider, I want to be able to view a patient?s appointment history so I can consult with the patient accordingly
+        # Acceptance Criteria:
+            # The patient?s appointment history should be accessed through their profile page
+            # A description of what occured in a specific appointment should be viewable, including any medication prescribed and notes taken at the previous visit
+  
+    # 3: Manage a patient history
+    
+# for vincent to start on    
