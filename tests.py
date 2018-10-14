98f8c709ec6c5051c83d983b725dd6230f57c019
Pipit21 <z5162791@student.unsw.edu.au>
2018-10-14 21:41:15 +1100

    new tests

--- tests.py
+++ tests.py
@@ -1,75 +1,159 @@
-import unittest
+from userManager import UserManager
+from centreManager import CentreManager
+from appointmentManager import AppointmentManager
 import pytest
-from routes import login
-from flask.ext.testing import TestCase
-# Currently fiddling around with test cases to fully understand how to implement
-# https://github.com/realpython/discover-flask/blob/master/tests/base.py
-# https://github.com/realpython/discover-flask/blob/master/tests/base.py
-# structure based on above links
+class Tests(object):
+	def setup_method(self):
+		self.centreManager = CentreManager()
+		self.userManager = UserManager(self.centreManager)
+		self.appointmentManager = AppointmentManager()
+		self.centreManager.addProviders(self.userManager)
 
-class my_test(TestCase):
-    app = Flask(__name__)
-    app.config['TESTING'] = True
-    return app
+# (1) BOOK AN APPOINTMENT
+	# 4 
+			
+	# (i) Testing that the appointment added is in the patient class and the provider class
+	def test_book_appointment(self): ##
+		self.appointmentManager.addAppointment(self.userManager, "toby@gmail.com",
+			"jack@gmail.com",  "00.00-00.30", "12/31/2018","Biopsy Required")
 
-class test_cases(my_test):
-    
-    # tests that flask is set up correct
-    def test_user_login(self):
-        #login_user(user)        
-        #response = self.client.get('/login', content_type='html/text')
-        #self.assertEqual(response.status_code, 200)
-        
-    # Ensure that main page requires user login
-    def test_main_route_requires_login(self):
-        # response = self.client.get('/', follow_redirects=True)
-        # the function supposedly tests for the texts on the screen
-        # self.assertIn(b'Log-in to your account', response.data)
-        
-    # Ensure that welcome page loads
-    def test_welcome_route_works_as_expected(self):
-    
-    
-    def test_book_an_appointment(self):
-    
-    
-    def test_view_patient_history(self):
-    
-    
-    def test_manage_patient_history(self):
-    
-    
-    
-if __name__ == '__main__':
-    unittest.main()
-        
-#this will be where tests for user stories are conducted
-# According to the google doc
-    # 1: Book and Appointment
-    # US7
-    # "User-Story Description:
-        # As a patient, I want to book an appointment so I can receive healthcare service easily
-        # Acceptance Criteria:
-            # Enter required information including Medicare number into a form
-            # Be able to book an appointment through a healthcare provider?s or centre?s profile page
-            # Have an option of 48 equally sized time slots, each of which is 30 minutes long
-            # Be able to enter brief information for the appointment
-            # Be able to display a confirmation that the appointment has been booked to the user          
+		# Check that the appointment is in the provider class
+		provider = self.userManager.getID("toby@gmail.com")
+		providerAppointment = provider.getSpecificAppointment(0)
+		assert(providerAppointment.provider == "toby@gmail.com")
+		assert(providerAppointment.patient == "jack@gmail.com")
+		assert(providerAppointment.date == "12/31/2018")
+		assert(providerAppointment.time == "00.00-00.30")
+		assert(providerAppointment.reason == "Biopsy Required")
+		
+		# Check that the appointment is in the patient class
+		patient = self.userManager.getID("jack@gmail.com")
+		patientAppointment = patient.getSpecificAppointment(0)
+		assert(patientAppointment.provider == "toby@gmail.com")
+		assert(patientAppointment.patient == "jack@gmail.com")
+		assert(patientAppointment.date == "12/31/2018")
+		assert(patientAppointment.time == "00.00-00.30")
+		assert(patientAppointment.reason == "Biopsy Required")  
+		
+	# (ii) Check that booking an appointment in the same timeslot does not work
+	def test_book_same_time_slot_appointment(self): ##
+		self.appointmentManager.addAppointment(self.userManager, "toby@gmail.com",
+			"jack@gmail.com",  "00.00-00.30", "12/31/2018", "Influenza")
 
-    # 2: View Patient History
-    # US13
-    # User-Story Description:
-        # As a patient, I want to be able to view my own appointments history so I can easily access which appointments I have recently attended
-        # Acceptance Criteria:
-            # The list of appointments should have links to the profile pages of the corresponding health provider and healthcare centre
-            # The list of appointments should be chronological and listed with the date of appointment
-            
-    # US15
-    # User-Story Description:
-        # As a healthcare provider, I want to be able to view a patient?s appointment history so I can consult with the patient accordingly
-        # Acceptance Criteria:
-            # The patient?s appointment history should be accessed through their profile page
-            # A description of what occured in a specific appointment should be viewable, including any medication prescribed and notes taken at the previous visit
-  
-    # 3: Manage a patient history
-   
+		status = self.appointmentManager.addAppointment(self.userManager, "toby@gmail.com",
+			"hao@gmail.com",  "00.00-00.30", "12/31/2018","Same Time slot")
+
+		assert(status == "Time Slot Taken")
+	
+	# (iii) provider makes an appointment for himself/herself
+	def test_provider_makes_appointment(self): ##
+		status = self.appointmentManager.addAppointment(self.userManager, "toby@gmail.com",
+			"toby@gmail.com", "00.00-00.30", "12/1/2018", "Arthiritis")
+
+		assert(status == "Provider making appointment")
+
+	# (additional) Check that you cannot book an appointment in the past
+	def test_book_past_appointment(self): ##
+		status = self.appointmentManager.addAppointment(self.userManager, "toby@gmail.com",
+			"jack@gmail.com",  "00.00-00.30", "09/24/2018","Constipation")
+
+		assert(status == "Date in the past")
+
+# (2) VIEW PATIENT HISTORY	
+	# 2	
+	# testing that the history is saved and can be accessed
+	def test_get_appointment_history(self): ##
+		self.appointmentManager.addAppointment(self.userManager, "toby@gmail.com",
+			"jack@gmail.com",  "00.00-00.30", "12/31/2018", "High Blood Pressure")
+
+		self.appointmentManager.addAppointment(self.userManager, "toby@gmail.com",
+			"hao@gmail.com",  "00.30-01.00", "12/31/2018","Vomiting")
+
+		user = self.userManager.getID("toby@gmail.com")
+		assert(len(self.appointmentManager.getAppointments(user)) == 2) 
+
+	# test updating with multiple people booking and 2 wanting the same time
+	def multiple_appointments_clash(self):
+		self.appointmentManager.addAppointment(self.userManager, "toby@gmail.com",
+			"jack@gmail.com",  "04.00-04.30", "12/31/2018","Biopsy Required")
+		
+		self.appointmentManager.addAppointment(self.userManager, "toby@gmail.com", 
+			"tom@gmail.com", "00.30-01.00", "12/31/2018", "Mole on foot")
+
+		# this appointment has the same time and date as the previous appointment   
+		clash = self.appointmentManager.addAppointment(self.userManager, "toby@gmail.com",
+			"hao@gmail.com",  "00.30-01.00", "12/31/2018","Mole on foot")
+			
+		assert(clash == "Time Slot Taken")
+
+		user = self.userManager.getID("toby@gmail.com") 
+		assert(len(self.appointmentManager.getAppointments(user)) == 1)	 
+	
+# (3) MANAGE A PATIENT HISTORY
+	# 4	
+	# test to ensure the provider can update appointment with notes and medicine prescribed 
+	def notes_and_medicine(self):
+		self.appointmentManager.addAppointment(self.userManager, "toby@gmail.com",
+			"jack@gmail.com",  "09.00-09.30", "11/23/2018","Biopsy Required")    
+			
+		user = self.userManager.getID("jack@gmail.com")
+		appointment = self.appointmentManager.getAppointmentUsingDate(user, 
+			"11/23/2018", "09.00-09.30")
+			
+		notes = "Baby Shark"
+		medicine = "Doo doo doo doo"
+		
+		self.appointmentManager.updateAppointment(appointment, notes, medicine)
+		
+		assert(appointment.notes == "Baby Shark")
+		assert(appointment.prescribedMedicine == "Doo doo doo doo")	 
+		
+	# updating notes from a previously inputted notes and medicine
+	def test_update_appointment(self): ##
+		self.appointmentManager.addAppointment(self.userManager, "toby@gmail.com",
+			"jack@gmail.com",  "09.00-09.30", "11/23/2018", "Biopsy Required")
+
+		user = self.userManager.getID("jack@gmail.com")
+		appointment = self.appointmentManager.getAppointmentUsingDate(user, 
+			"11/23/2018", "09.00-09.30")
+			
+		appointment.notes = "This person did not require a doctor"
+		appointment.prescribedMedicine = "Obecalp pills"
+		
+		newNotes = "I was wrong"
+		newMedicine = "Sugar"
+		
+		self.appointmentManager.updateAppointment(appointment, newNotes, newMedicine)
+		assert(appointment.notes == "This person did not require a doctor\nI was wrong")
+		assert(appointment.prescribedMedicine == "Obecalp pills\nSugar")
+		
+	# updating notes then realising no updates are needed -> NULL submission          
+	def test_update_appointment_with_nothing(self): ##
+		self.appointmentManager.addAppointment(self.userManager, "toby@gmail.com",
+			"jack@gmail.com",  "08.00-08.30", "11/23/2018", "Biopsy Required")
+
+		user = self.userManager.getID("jack@gmail.com")
+		appointment = self.appointmentManager.getAppointmentUsingDate(user, 
+			"11/23/2018", "08.00-08.30")
+		appointment.notes = "This person did not require a doctor"
+		appointment.prescribedMedicine = "Obecalp pills"
+		
+		newNotes = ""
+		newMedicine = ""
+		
+		self.appointmentManager.updateAppointment(appointment, newNotes, newMedicine)
+		assert(appointment.notes == "This person did not require a doctor\n")
+		assert(appointment.prescribedMedicine == "Obecalp pills\n")
+			
+	# Test getting the history with an email that does not have any appointments    
+	def test_get_wrong_id_history(self): ##
+		self.appointmentManager.addAppointment(self.userManager, "toby@gmail.com",
+			"jack@gmail.com", "00.00-00.30", "12/31/2018", "Biopsy Required")
+
+		self.appointmentManager.addAppointment(self.userManager, "toby@gmail.com", 
+			"hao@gmail.com", "00.30-01.00", "12/31/2018", "Mole on foot")
+		
+		user = self.userManager.getID("gary@gmail.com")
+		assert(len(self.appointmentManager.getAppointments(user)) == 0)
+		
+		
